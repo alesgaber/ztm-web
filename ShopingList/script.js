424d252c334c0415528f8bd86095a4b14f469375
alesgaber <ales.gaber@gmail.com>
2021-01-08 14:58:25 +0100

    semicolon added

--- ShopingList/script.js
+++ ShopingList/script.js
@@ -1,63 +1,63 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var button = document.getElementById('enter')
+var input = document.getElementById('userinput')
+var ul = document.querySelector('ul')
 
-function inputLength() {
-	return input.value.length;
+function inputLength () {
+  return input.value.length
 }
 
-function createListElement() {
-	var li = document.createElement("li");	
-	li.appendChild(document.createTextNode(input.value));
-	
-	//add button to li element
-	var button  = createButtonElement();
-	li.appendChild(button);
+function createListElement () {
+  var li = document.createElement('li')
+  li.appendChild(document.createTextNode(input.value))
 
-	ul.appendChild(li);
-	li.addEventListener("click", addClassAfterClick)
-	input.value = "";
+  // add button to li element
+  var button = createButtonElement()
+  li.appendChild(button)
+
+  ul.appendChild(li)
+  li.addEventListener('click', addClassAfterClick)
+  input.value = ''
 }
 
-function createButtonElement() {
-	var b = document.createElement('button');
-	b.appendChild(document.createTextNode('Done!'));
-	b.addEventListener('click', deleteListItem)
+function createButtonElement () {
+  var b = document.createElement('button')
+  b.appendChild(document.createTextNode('Done!'))
+  b.addEventListener('click', deleteListItem)
 
-	return b;
+  return b
 }
 
-function deleteListItem(e) {
-	ul.removeChild(e.target.parentElement);
+function deleteListItem (e) {
+  ul.removeChild(e.target.parentElement)
 }
 
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+function addListAfterClick () {
+  if (inputLength() > 0) {
+    createListElement();
+  }
 }
 
-function addClickEventToExistingList(){ 
-	ul.childNodes.forEach (function(el){
-		el.addEventListener("click", addClassAfterClick);
-	}			);
+function addClickEventToExistingList () {
+  ul.childNodes.forEach(function (el) {
+    el.addEventListener('click', addClassAfterClick)
+  });
 }
 
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+function addListAfterKeypress (event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement()
+  }
 }
 
-function addClassAfterClick(e) {		
-	addClass(e.target, 'done');
+function addClassAfterClick (e) {
+  addClass(e.target, 'done');
 }
 
-function addClass(el, className) {
-	el.classList.add(className);
+function addClass (el, className) {
+  el.classList.add(className);
 }
 
-button.addEventListener("click", addListAfterClick);
-input.addEventListener("keypress", addListAfterKeypress);
+button.addEventListener('click', addListAfterClick);
+input.addEventListener('keypress', addListAfterKeypress);
 
 addClickEventToExistingList();
