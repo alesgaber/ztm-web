3faee5b518511fc01c35585edd752f72a08c0758
alesgaber <ales.gaber@gmail.com>
2021-01-15 09:22:39 +0100

    added Error boundaries

---
+++ robofriends/src/components/ErrorBoundry.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+
+class ErrorBoundry extends Component {
+
+    constructor(props) {
+        super();
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch(){
+        this.setState({hasError: true});
+    }
+
+    render(){
+        if (this.state.hasError){
+            return <h1>Oooops. Taht is not good</h1>
+        }
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundry 
